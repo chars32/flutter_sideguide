ddd88094a44a2592107c97f5fdf9d26b0cd831f1
Frontend <frontend@healonsite.mx>
2022-06-21 18:34:41 -0500

    abstract

---
+++ 3.Introducion Dart/abstract_classes.dart
@@ -0,0 +1,36 @@
+abstract class Test {
+  /// This is an abstract method because it has no body ({}).
+  void testOne();
+
+  /// This is NOT an abstract method because it has a body.
+  void testTwo() {}
+
+  // No constructor as well
+}
+
+// Using extends
+// class UnitTest extends Test {
+//   @override
+//   void testOne() {
+//     print('hello!');
+//   }
+// }
+
+// Using implements
+class UnitTest implements Test {
+  @override
+  void testOne() {
+    print('hello one!');
+  }
+
+  @override
+  void testTwo() {
+    print('hello two!');
+  }
+}
+
+void main() {
+  UnitTest test = UnitTest();
+  test.testOne(); // hello!
+  test.testTwo();// hello!
+}
