a8bd6d0ae9cc8e1f276a901b9f81512b02f90880
Frontend <frontend@healonsite.mx>
2022-06-22 00:39:59 -0500

    Maps

---
+++ 3.Introducion Dart/maps.dart
@@ -0,0 +1,9 @@
+void main() {
+  final dictionary = {
+    "love": "an intense feeling of deep affection.",
+    "hate": "feel intense or passionate dislike"
+  };
+
+  final definition = dictionary["love"];
+  print(definition); //
+}
