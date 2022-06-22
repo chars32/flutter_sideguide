983ed9f56cf8bb52fd31850da25f614b63f5a211
Frontend <frontend@healonsite.mx>
2022-06-22 14:11:43 -0500

    5. Changing text styles

--- 4.Your first Flutter app/5.Portfolio/my_portfolio/lib/pages/home_page.dart
+++ 4.Your first Flutter app/5.Portfolio/my_portfolio/lib/pages/home_page.dart
@@ -8,20 +8,27 @@ class HomePage extends StatelessWidget {
     return Scaffold(
       body: Center(
         child: Container(
+          margin: const EdgeInsets.all(25.00),
           padding: const EdgeInsets.all(25.0),
           decoration: BoxDecoration(
-            color: Colors.white,
-            boxShadow: [
-              BoxShadow(
-                color: Colors.black.withOpacity(0.15),
-                offset: const Offset(0, 15),
-                blurRadius: 25
-              )
-            ],
-            border: Border.all(color: Colors.black, width: 1),
-            borderRadius: BorderRadius.circular(15.0)
+              color: Colors.white,
+              boxShadow: [
+                BoxShadow(
+                    color: Colors.black.withOpacity(0.15),
+                    offset: const Offset(0, 15),
+                    blurRadius: 25)
+              ],
+              border: Border.all(color: Colors.black, width: 1),
+              borderRadius: BorderRadius.circular(15.0)),
+          child: Text(
+            "Welcome! My name is Carlos Garcia Castellanos",
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              color: Colors.grey.shade900,
+              fontSize: 20,
+              fontWeight: FontWeight.bold
+            ),
           ),
-          child: const Text("Werlcome! My name is Chars"),
         ),
       ),
     );
