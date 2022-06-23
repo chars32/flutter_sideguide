2d1c1765d8be021c32b46e0f780b4b29fae59737
Frontend <frontend@healonsite.mx>
2022-06-22 23:46:08 -0500

    5.Making projectcard reusable

--- 5.Portfolio/my_portfolio/lib/pages/home_page.dart
+++ 5.Portfolio/my_portfolio/lib/pages/home_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:my_portfolio/widgets/Cards/project_card.dart';
 import 'package:my_portfolio/widgets/header.dart';
-import 'package:my_portfolio/widgets/intro_card.dart';
+// import 'package:my_portfolio/widgets/intro_card.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -21,81 +22,16 @@ class HomePage extends StatelessWidget {
               SizedBox(height: 25),
               Text('Projects',
                   style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold)),
-              ProjectCard(),
+              ProjectCard(
+                projectName: "My Project",
+                projectYear: 2022,
+                projectDescription:
+                    "This is the project's description. You can write your own description here.",
+              ),
             ],
           ),
         ),
       ),
     ));
-  }
-}
-
-class ProjectCard extends StatelessWidget {
-  const ProjectCard({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      height: 350,
-      padding: const EdgeInsets.all(15.0),
-      decoration: BoxDecoration(
-          color: Colors.white,
-          borderRadius: BorderRadius.circular(15.0),
-          boxShadow: [
-            BoxShadow(
-              offset: const Offset(5, 15),
-              color: Colors.black.withOpacity(.15),
-              blurRadius: 15,
-            )
-          ]),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              const Text(
-                "My Project",
-                style: TextStyle(
-                  fontSize: 18,
-                ),
-              ),
-              Container(
-                decoration: BoxDecoration(
-                    color: Colors.black,
-                    borderRadius: BorderRadius.circular(15.0)),
-                padding: const EdgeInsets.all(5.0),
-                child: const Text(
-                  "2022",
-                  style: TextStyle(fontSize: 14, color: Colors.white),
-                ),
-              ),
-            ],
-          ),
-          const SizedBox(
-            height: 15,
-          ),
-          Expanded(
-            child: ClipRRect(
-              borderRadius: BorderRadius.circular(15.0),
-              child: Image.network(
-                "https://images.unsplash.com/photo-1551288049-bebda4e38f71?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80",
-                fit: BoxFit.cover,
-              ),
-            ),
-          ),
-          const SizedBox(
-            height: 10,
-          ),
-          const Text(
-            """Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime sint commodi repudiandae consequuntur voluptatum laborum
-numquam blanditiis harum""",
-            style: TextStyle(
-              fontSize: 14,
-            ),
-          ),
-        ],
-      ),
-    );
   }
 }
