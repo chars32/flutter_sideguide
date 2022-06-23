fbacede687fb1eef4bd021d8652b27fdde83ee04
Frontend <frontend@healonsite.mx>
2022-06-23 16:49:04 -0500

    6.Building the technologies list widget

--- 5.Portfolio/my_portfolio/lib/example/projects.dart
+++ 5.Portfolio/my_portfolio/lib/example/projects.dart
@@ -3,24 +3,28 @@ import 'package:my_portfolio/models/project.dart';
 List<Project> projects = [
   Project(
       name: "Project 1",
+      technologiesUsed: ["Flutter", "AWS", "Git"],
       year: 2018,
       description: "This is my description 1",
       imageUrl:
           "https://images.unsplash.com/photo-1551288049-bebda4e38f71?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80"),
   Project(
       name: "Project 2",
+      technologiesUsed: ["Next.js", "Firebase"],
       year: 2019,
       description: "This is my description 2",
       imageUrl:
           "https://images.unsplash.com/photo-1551288049-bebda4e38f71?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80"),
   Project(
       name: "Project 3",
+      technologiesUsed: ["Flutter", "Firebase"],
       year: 2020,
       description: "This is my description 3",
       imageUrl:
           "https://images.unsplash.com/photo-1551288049-bebda4e38f71?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80"),
   Project(
       name: "Project 4",
+      technologiesUsed: ["Next.js", "OAuth"],
       year: 2021,
       description: "This is my description 4",
       imageUrl:
