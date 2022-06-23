961529d529d9405764d5b27067c5429a07e1ff6a
Frontend <frontend@healonsite.mx>
2022-06-22 19:03:45 -0500

    5. Project Card v1

--- 4.Your first Flutter app/5.Portfolio/my_portfolio/lib/pages/home_page.dart
+++ 4.Your first Flutter app/5.Portfolio/my_portfolio/lib/pages/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:my_portfolio/widgets/header.dart';
 import 'package:my_portfolio/widgets/intro_card.dart';
 
 class HomePage extends StatelessWidget {
@@ -15,38 +16,69 @@ class HomePage extends StatelessWidget {
           padding: const EdgeInsets.all(15.0),
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Row(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    mainAxisSize: MainAxisSize.min,
-                    children: const [
-                      Text(
-                        "Welcome back",
-                        style: TextStyle(
-                            fontSize: 20, fontWeight: FontWeight.w300),
-                      ),
-                      Text(
-                        "Chars's Portfolio",
-                        style: TextStyle(fontWeight: FontWeight.bold),
-                      )
-                    ],
-                  ),
-                  const Icon(
-                    Icons.account_circle,
-                    color: Colors.black,
-                    size: 30,
-                  ),
-                ],
-              )
+            children: const [
+              Header(),
+              SizedBox(height: 25),
+              Text('Projects',
+                  style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold)),
+              ProjectCard(),
             ],
           ),
         ),
       ),
     ));
+  }
+}
+
+class ProjectCard extends StatelessWidget {
+  const ProjectCard({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 350,
+      padding: const EdgeInsets.all(15.0),
+      decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(15.0),
+          boxShadow: [
+            BoxShadow(
+              offset: const Offset(5, 15),
+              color: Colors.black.withOpacity(.15),
+              blurRadius: 15,
+            )
+          ]),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              const Text(
+                "My Project",
+                style: TextStyle(
+                  fontSize: 18,
+                ),
+              ),
+              Container(
+                decoration: BoxDecoration(
+                    color: Colors.black,
+                    borderRadius: BorderRadius.circular(15.0)),
+                padding: const EdgeInsets.all(5.0),
+                child: const Text(
+                  "2022",
+                  style: TextStyle(fontSize: 14, color: Colors.white),
+                ),
+              ),
+            ],
+          ),
+          const SizedBox(
+            height: 15,
+          ),
+          Image.network("https://images.unsplash.com/photo-1551288049-bebda4e38f71?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80",
+          fit: BoxFit.cover,)
+        ],
+      ),
+    );
   }
 }
