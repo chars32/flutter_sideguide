31b0e6d8a36ed5872ec43af98421b6fbc8716cdb
Frontend <frontend@healonsite.mx>
2022-06-24 23:48:25 -0500

    7. Stateful: Add transaction card on button click

---
+++ 7.Budget tracker/budject_tracker_project/lib/models/transaction_item.dart
@@ -0,0 +1,8 @@
+class TransactionItem {
+  String itemTitle;
+  double amount;
+  bool isExpense;
+
+  TransactionItem(
+      {required this.amount, required this.itemTitle, this.isExpense = true});
+}
