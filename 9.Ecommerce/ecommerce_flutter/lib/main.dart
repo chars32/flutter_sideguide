6c606b78f1492257062a82d0a4f79fdfc4322ed9
Frontend <frontend@healonsite.mx>
2022-06-29 22:08:56 -0500

    9.Riverpod: State Management

--- 9.Ecommerce/ecommerce_flutter/lib/main.dart
+++ 9.Ecommerce/ecommerce_flutter/lib/main.dart
@@ -1,27 +1,57 @@
 import 'package:ecommerce_flutter/firebase_options.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
-  runApp(const MyApp());
+  runApp(
+    const ProviderScope(
+      child: MyApp(),
+    ),
+  );
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends ConsumerWidget {
   const MyApp({Key? key}) : super(key: key);
 
   // This widget is the root of your application.
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final count = ref.watch(counterProvider);
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const Scaffold(),
+      home: Scaffold(
+        body: Center(
+          child: Text(
+            count.toString(),
+          ),
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            ref.read(counterProvider.notifier).increment();
+          },
+          child: const Icon(Icons.add),
+        ),
+      ),
     );
   }
+}
+
+final counterProvider = StateNotifierProvider(
+  (ref) {
+    return Counter();
+  },
+);
+
+class Counter extends StateNotifier<int> {
+  Counter() : super(0);
+
+  void increment() => state++;
 }
