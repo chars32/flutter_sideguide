87ad97fc12cc92be10ba775098c697840f50401e
Frontend <frontend@healonsite.mx>
2022-07-05 23:06:33 -0500

    10.Displaying the products in the user app

--- 9.Ecommerce/ecommerce_flutter/lib/app/pages/user/user_home.dart
+++ 9.Ecommerce/ecommerce_flutter/lib/app/pages/user/user_home.dart
@@ -1,5 +1,6 @@
 import 'package:ecommerce_flutter/app/providers.dart';
 import 'package:ecommerce_flutter/widgets/product_banner.dart';
+import 'package:ecommerce_flutter/widgets/products_display.dart';
 import 'package:ecommerce_flutter/widgets/user_top_bar.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
@@ -30,6 +31,19 @@ class UserHome extends ConsumerWidget {
               const SizedBox(height: 20),
               const ProductBanner(),
               const SizedBox(height: 20),
+              const Text(
+                "Products",
+                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+              ),
+              const Text(
+                "View all of our products",
+                style: TextStyle(fontSize: 12),
+              ),
+              // Flexible widget prevent overflow layout it will use the
+              // amount of space available
+              const Flexible(
+                child: ProductsDisplay(),
+              ),
             ],
           ),
         ),
